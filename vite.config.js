becd276265807b29bff56b0ca75ad9efbb2db9fb
Mohammed7134 <61147742+Mohammed7134@users.noreply.github.com>
2022-09-21 19:56:50 +0300

    vite changes

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,13 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     server: {
-        https: true
+        https: true,
+        host: 'localhost',
     },
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
+                'resources/sass/app.scss',
                 'resources/js/app.js',
             ],
             refresh: true,
