bbfdfec024b090ef7d9805ffa8142b920a780be9
Mohammed7134 <61147742+Mohammed7134@users.noreply.github.com>
2022-09-21 20:17:27 +0300

    test vite

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    server: {
-        https: true,
-        host: 'localhost',
-    },
     plugins: [
         laravel({
             input: [
