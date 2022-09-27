a81c6655558a8184f91ef7abf474a06a05cbbc2f
Mohammed7134 <61147742+Mohammed7134@users.noreply.github.com>
2022-09-27 20:28:44 +0300

    e

--- app/Http/Controllers/WisdomController.php
+++ app/Http/Controllers/WisdomController.php
@@ -24,6 +24,7 @@ class WisdomController extends Controller
     }
     public function index()
     {
+
         $wisdoms = Wisdom::inRandomOrder()->paginate(7);
         if (request()->ajax()) {
             return $this->ajax($wisdoms);
