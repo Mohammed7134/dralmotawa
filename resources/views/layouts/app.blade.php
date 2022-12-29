d2ea9efb20f71061bc5a65b7010be2a21a3b7363
Mohammed7134 <61147742+Mohammed7134@users.noreply.github.com>
2022-12-29 13:28:23 +0300

    GA4 user login test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,14 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ Session::token() }}">
-    @auth
-    <script>
-        dataLayer.push({
-            'event':'login',
-            'userId' : '123abd'
-        });
-    </script>
-    @endauth
     <!-- Google Tag Manager -->
     <script>
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
