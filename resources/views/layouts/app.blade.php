f1935ad7bf49a28f5fe44079a94431d21d15e912
Mohammed7134 <61147742+Mohammed7134@users.noreply.github.com>
2022-12-31 08:06:57 +0300

    dataLayer in head just before GTM

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
 <html lang="ar" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ Session::token() }}">
     @auth
     <script>
         dataLayer.push({
@@ -8,12 +13,6 @@
         });
     </script>
     @endauth
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="csrf-token" content="{{ Session::token() }}">
-
     <!-- Google Tag Manager -->
     <script>
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
