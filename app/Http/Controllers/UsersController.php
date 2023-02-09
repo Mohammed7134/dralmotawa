11b0730944dcb5831451a14497a58f73d0d15ed4
Mohammed7134 <61147742+Mohammed7134@users.noreply.github.com>
2023-02-09 20:59:18 +0300

    test2

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -129,7 +129,7 @@ class UsersController extends Controller
     function messageFromTwilio()
     {
 
-        $data = json_decode(request()->getContent(), true);
+        $data = json_decode(request(), true);
         Log::info('This is an informational message.' . $data);
         return;
         if ($data['field'] === 'messages') {
