1a45b4715fb9d916ca36b51d3e6e49a087fad32e
Mohammed7134 <61147742+Mohammed7134@users.noreply.github.com>
2023-02-10 13:36:28 +0300

    trial 5

--- app/Console/Commands/dailyWisdom.php
+++ app/Console/Commands/dailyWisdom.php
@@ -44,10 +44,10 @@ class dailyWisdom extends Command
             );
             $component1 = json_encode(array(
                 "type" => "body",
-                "parameters" => ['{
+                "parameters" => [json_encode('{
                         "type": "text",
                         "text":' . $wisdom . '
-                    }']
+                    }')]
             ));
             $body = ["messaging_product" => "whatsapp", "to" => $subscriber->telephone, "type" => "template", "template" => ["name" => "wisdom", "language" => ["code" => "ar"], "components" => [$component1]]];
 
