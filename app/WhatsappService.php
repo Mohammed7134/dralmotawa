8c20a87c73554fbeceb49458746964d5aa8fed62
Mohammed7134 <61147742+Mohammed7134@users.noreply.github.com>
2023-02-10 16:50:17 +0300

    service update

---
+++ app/WhatsappService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Subscriber;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Stream;
+
+function sendWhatsApp(Subscriber $subscriber, String $parameter1, String $templateName)
+{
+    $client = new Client();
+    $uri = getenv('WHATSAPP_URI');
+    $headers = array(
+        'Content-Type' => 'application/json',
+        'Authorization' => getenv('WHATSAPP_TOKEN')
+    );
+    $component1 = json_encode(array(
+        "type" => "body",
+        "parameters" => [$parameter1]
+    ));
+    $body = ["messaging_product" => "whatsapp", "to" => $subscriber->telephone, "type" => "template", "template" => ["name" => $templateName, "language" => ["code" => "ar"], "components" => [$component1]]];
+
+    $request = new Request('POST', $uri, $headers);
+    $stream = new Stream(fopen('php://temp', 'r+'));
+    $stream->write(json_encode($body));
+    $stream->rewind();
+    $request = $request->withBody($stream);
+    $response = $client->send($request);
+}
--- app/helpers.php
+++ app/helpers.php
@@ -1,48 +1,16 @@
 <?php
-
-namespace App\Services;
-
-use App\Models\Subscriber;
-use GuzzleHttp\Client;
-use GuzzleHttp\Psr7\Request;
-use GuzzleHttp\Psr7\Stream;
-
-class MyService
+function adjustLineBreaks($wisdom, $twitter)
 {
-    public function adjustLineBreaks($wisdom, $twitter)
-    {
-        if ($twitter) {
-            str_replace(['&nbsp;', '/\xc2\xa0/', '\\u00a0"'], " ", $wisdom);
-            str_replace(array("\r\n", "\r", "\n"), "<br />", $wisdom);
-            $wisdom = nl2br($wisdom, true);
-            $wisdom = str_replace("<br />", "%0A", $wisdom);
-            echo $wisdom;
-        } else {
-            str_replace(['&nbsp;', '/\xc2\xa0/', '\\u00a0"'], " ", $wisdom);
-            str_replace(array("\r\n", "\r", "\n"), "<br />", $wisdom);
-            str_replace("<br>", "<br />", $wisdom);
-            echo nl2br($wisdom, false);
-        }
-    }
-    public function sendWhatsApp(Subscriber $subscriber, String $parameter1, String $templateName)
-    {
-        $client = new Client();
-        $uri = getenv('WHATSAPP_URI');
-        $headers = array(
-            'Content-Type' => 'application/json',
-            'Authorization' => getenv('WHATSAPP_TOKEN')
-        );
-        $component1 = json_encode(array(
-            "type" => "body",
-            "parameters" => [$parameter1]
-        ));
-        $body = ["messaging_product" => "whatsapp", "to" => $subscriber->telephone, "type" => "template", "template" => ["name" => $templateName, "language" => ["code" => "ar"], "components" => [$component1]]];
-
-        $request = new Request('POST', $uri, $headers);
-        $stream = new Stream(fopen('php://temp', 'r+'));
-        $stream->write(json_encode($body));
-        $stream->rewind();
-        $request = $request->withBody($stream);
-        $response = $client->send($request);
+    if ($twitter) {
+        str_replace(['&nbsp;', '/\xc2\xa0/', '\\u00a0"'], " ", $wisdom);
+        str_replace(array("\r\n", "\r", "\n"), "<br />", $wisdom);
+        $wisdom = nl2br($wisdom, true);
+        $wisdom = str_replace("<br />", "%0A", $wisdom);
+        echo $wisdom;
+    } else {
+        str_replace(['&nbsp;', '/\xc2\xa0/', '\\u00a0"'], " ", $wisdom);
+        str_replace(array("\r\n", "\r", "\n"), "<br />", $wisdom);
+        str_replace("<br>", "<br />", $wisdom);
+        echo nl2br($wisdom, false);
     }
 }
