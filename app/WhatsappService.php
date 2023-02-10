c601d2fbc7ee02d96fbebb7231a2c89bf4ebccb8
Mohammed7134 <61147742+Mohammed7134@users.noreply.github.com>
2023-02-10 16:55:28 +0300

    update Whatsapp Service

--- app/WhatsappService.php
+++ app/WhatsappService.php
@@ -7,24 +7,27 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Stream;
 
-function sendWhatsApp(Subscriber $subscriber, String $parameter1, String $templateName)
+class MyService
 {
-    $client = new Client();
-    $uri = getenv('WHATSAPP_URI');
-    $headers = array(
-        'Content-Type' => 'application/json',
-        'Authorization' => getenv('WHATSAPP_TOKEN')
-    );
-    $component1 = json_encode(array(
-        "type" => "body",
-        "parameters" => [$parameter1]
-    ));
-    $body = ["messaging_product" => "whatsapp", "to" => $subscriber->telephone, "type" => "template", "template" => ["name" => $templateName, "language" => ["code" => "ar"], "components" => [$component1]]];
+    function sendWhatsApp(Subscriber $subscriber, String $parameter1, String $templateName)
+    {
+        $client = new Client();
+        $uri = getenv('WHATSAPP_URI');
+        $headers = array(
+            'Content-Type' => 'application/json',
+            'Authorization' => getenv('WHATSAPP_TOKEN')
+        );
+        $component1 = json_encode(array(
+            "type" => "body",
+            "parameters" => [$parameter1]
+        ));
+        $body = ["messaging_product" => "whatsapp", "to" => $subscriber->telephone, "type" => "template", "template" => ["name" => $templateName, "language" => ["code" => "ar"], "components" => [$component1]]];
 
-    $request = new Request('POST', $uri, $headers);
-    $stream = new Stream(fopen('php://temp', 'r+'));
-    $stream->write(json_encode($body));
-    $stream->rewind();
-    $request = $request->withBody($stream);
-    $response = $client->send($request);
+        $request = new Request('POST', $uri, $headers);
+        $stream = new Stream(fopen('php://temp', 'r+'));
+        $stream->write(json_encode($body));
+        $stream->rewind();
+        $request = $request->withBody($stream);
+        $response = $client->send($request);
+    }
 }
