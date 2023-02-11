71c639603455ca8b218da6e238cafca2ccc51196
Mohammed7134 <61147742+Mohammed7134@users.noreply.github.com>
2023-02-11 08:19:54 +0300

    adjusted daily message time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('Wisdom:day')->dailyAt('15:04');
+        $schedule->command('Wisdom:day')->dailyAt('05:25');
     }
 
     /**
