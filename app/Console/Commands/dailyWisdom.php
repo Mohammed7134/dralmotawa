26066b30222f6ca8ae1eafdfac9ed9f63bc3465d
Mohammed7134 <61147742+Mohammed7134@users.noreply.github.com>
2023-02-24 10:56:00 +0300

    test

--- app/Console/Commands/dailyWisdom.php
+++ app/Console/Commands/dailyWisdom.php
@@ -43,7 +43,25 @@ class dailyWisdom extends Command
             Log::debug(print_r($date, true));
             Log::debug($date->addDays($subscriber->payments->last()->period)->isPast() == true);
 
-            if ($date->addDays($subscriber->payments->last()->period)->isPast() == false) {
+            if ($date->addDays($subscriber->payments->last()->period + 1)->isToday()) {
+                if ($subscriber->payments->last()->status == "CAPTURED") {
+                    $parameter1 = json_encode(array(
+                        "type" => "text",
+                        "text" => "https://dralmutawa.com/renew-subscription/" . $subscriber->id . "?period=30"
+                    ));
+                    $parameter2 = json_encode(array(
+                        "type" => "text",
+                        "text" => "https://dralmutawa.com/renew-subscription/" . $subscriber->id . "?period=365"
+                    ));
+                    $response = $myservice->sendWhatsApp($subscriber, [$parameter1, $parameter2], 'renew_subscription_reminder');
+                    if (isset($response->error->message)) {
+                        Log::debug("One renew subscription message was not sent to $subscriber->id due to error");
+                        Log::debug($response->error->message);
+                    } else {
+                        Log::debug("One renew subscription message sent successfully to $subscriber->id");
+                    }
+                }
+            } elseif ($date->addDays($subscriber->payments->last()->period)->isPast() == false) {
                 if ($subscriber->payments->last()->status == "CAPTURED") {
                     $parameter1 = json_encode(array(
                         "type" => "text",
@@ -63,24 +81,6 @@ class dailyWisdom extends Command
                             "text" => $wisdom
                         ));
                         $response = $myservice->sendWhatsApp($subscriber, [$parameter1], 'wisdom');
-                    }
-                }
-            } elseif ($date->addDays($subscriber->payments->last()->period + 1)->isToday()) {
-                if ($subscriber->payments->last()->status == "CAPTURED") {
-                    $parameter1 = json_encode(array(
-                        "type" => "text",
-                        "text" => "https://dralmutawa.com/renew-subscription/" . $subscriber->id . "?period=30"
-                    ));
-                    $parameter2 = json_encode(array(
-                        "type" => "text",
-                        "text" => "https://dralmutawa.com/renew-subscription/" . $subscriber->id . "?period=365"
-                    ));
-                    $response = $myservice->sendWhatsApp($subscriber, [$parameter1, $parameter2], 'renew_subscription_reminder');
-                    if (isset($response->error->message)) {
-                        Log::debug("One renew subscription message was not sent to $subscriber->id due to error");
-                        Log::debug($response->error->message);
-                    } else {
-                        Log::debug("One renew subscription message sent successfully to $subscriber->id");
                     }
                 }
             }
