464514d1aeb8763340e20027486e92a6617855b8
Mohammed7134 <61147742+Mohammed7134@users.noreply.github.com>
2023-02-24 09:38:03 +0300

    test 700 length 1

--- app/Console/Commands/dailyWisdom.php
+++ app/Console/Commands/dailyWisdom.php
@@ -35,7 +35,7 @@ class dailyWisdom extends Command
     {
         $subscribers = Subscriber::all();
         // $wisdom = Wisdom::inRandomOrder()->first()->text;
-        $wisdom = Wisdom::where('id', '=', 36905)->first()->text;
+        $wisdom = Wisdom::where('id', '=', 44492)->first()->text;
         $myservice = new MyService;
         foreach ($subscribers as $subscriber) {
             $date = Carbon::parse($subscriber->payments->last()->created_at);
