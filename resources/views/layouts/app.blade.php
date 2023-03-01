deaee97e81b7d60a780b50431bfde6dc05f08840
Mohammed7134 <61147742+Mohammed7134@users.noreply.github.com>
2023-03-01 10:56:05 +0300

    favico.ico removed from app blade for testing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="csrf-token" content="{{ Session::token() }}">
         
-        <link rel="icon" href="favicon.ico" />
+        {{-- <link rel="icon" href="/favicon.ico" /> --}}
         
         <title>فقه الحياة | @yield('title')</title>
 
