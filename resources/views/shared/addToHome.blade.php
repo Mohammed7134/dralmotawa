56ef5f8e899107d575541d966da02833f643aefd
Mohammed7134 <61147742+Mohammed7134@users.noreply.github.com>
2023-06-15 23:32:26 +0300

    final touch

--- resources/views/shared/addToHome.blade.php
+++ resources/views/shared/addToHome.blade.php
@@ -1,7 +1,7 @@
 <div id="ios-add-to-home">
-    <p id="add-to-home-text" class="p-2">يجب إضافة الموقع إلى الصفحة الرئيسية أولا</p>
-    <div id="add-to-home-btn">
-        <i class="fa-solid fa-arrow-down arrow"></i>
-    </div>
-    <button id="dismiss-btn">X</button>
+    <p id="add-to-home-text">يجب إضافة الموقع إلى الصفحة الرئيسية أولا
+        <br>إنقر على <span><i class="fa-solid fa-arrow-up-from-bracket"></i></span> ثم إضافة
+        للقائمة الرئيسية <span><i class="fa-solid fa-square-plus"></i></span>
+    </p>
+    <button id="dismiss-btn">x</button>
 </div>
