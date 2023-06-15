b633b7310d9dbc542b442e13f574b815c9adabae
Mohammed7134 <61147742+Mohammed7134@users.noreply.github.com>
2023-06-15 18:58:25 +0300

    test 7

--- resources/views/shared/addToHome.blade.php
+++ resources/views/shared/addToHome.blade.php
@@ -1,7 +1,7 @@
 <div id="ios-add-to-home">
     <p id="add-to-home-text">يجب إضافة الموقع إلى الصفحة الرئيسية أولا</p>
-    <button id="add-to-home-btn" class="arrow-button">
+    <div id="add-to-home-btn">
         <i class="fa-solid fa-arrow-down arrow"></i>
-    </button>
+    </div>
     <button id="dismiss-btn">X</button>
 </div>
