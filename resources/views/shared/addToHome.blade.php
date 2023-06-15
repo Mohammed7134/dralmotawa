c70a34783bb4a083c87c8f9a235e76bdfc8e6561
Mohammed7134 <61147742+Mohammed7134@users.noreply.github.com>
2023-06-15 19:18:50 +0300

    test 8

--- resources/views/shared/addToHome.blade.php
+++ resources/views/shared/addToHome.blade.php
@@ -1,5 +1,5 @@
 <div id="ios-add-to-home">
-    <p id="add-to-home-text">يجب إضافة الموقع إلى الصفحة الرئيسية أولا</p>
+    <p id="add-to-home-text" class="p-2">يجب إضافة الموقع إلى الصفحة الرئيسية أولا</p>
     <div id="add-to-home-btn">
         <i class="fa-solid fa-arrow-down arrow"></i>
     </div>
