5be88c7754ec901f3ee2717128612bc4e3406126
Mohammed7134 <61147742+Mohammed7134@users.noreply.github.com>
2023-06-16 14:59:10 +0300

    test2

--- public/sw.js
+++ public/sw.js
@@ -62,7 +62,7 @@ self.addEventListener('push', function (e) {
 
     if (e.data) {
         var msg = e.data.json();
-        console.log(msg)
+        console.log(msg.data)
         e.waitUntil(
             self.registration.showNotification(msg.title, {
                 body: msg.body,
