84d88588988d1030a102de4bc107d93a86a3a9c3
Mohammed7134 <61147742+Mohammed7134@users.noreply.github.com>
2023-07-09 17:58:19 +0300

    Update app.blade.php
    Commit from phone

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0 shrink-to-fit=no">
         <meta name="csrf-token" content="{{ Session::token() }}">
 
-        <title>فقه الحياة | @yield('title')</title>
+        <title>فقه الحياةِ | @yield('title')</title>
 
         @isset($originalId)
         @include('layouts.googleTool')
