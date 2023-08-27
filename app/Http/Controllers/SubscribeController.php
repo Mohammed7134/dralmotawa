e57413eadd9aa531a31178be6443a95668dd6f7d
Mohammed7134 <61147742+Mohammed7134@users.noreply.github.com>
2023-08-27 18:18:38 +0300

    test push to guests

--- app/Http/Controllers/SubscribeController.php
+++ app/Http/Controllers/SubscribeController.php
@@ -35,8 +35,8 @@ class SubscribeController extends Controller
     }
     public function push()
     {
-        Notification::send(User::all(), new PushWisdom());
-        // Notification::send(Guest::all(), new PushWisdom(request()->id));
+        // Notification::send(User::all(), new PushWisdom());
+        Notification::send(Guest::all(), new PushWisdom(request()->id));
         return redirect()->back();
     }
 }
