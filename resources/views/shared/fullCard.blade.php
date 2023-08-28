ecfd4857e36b670b8be5e7085d5e3f209fa09a90
Mohammed7134 <61147742+Mohammed7134@users.noreply.github.com>
2023-08-28 11:02:04 +0300

    user no select

--- resources/views/shared/fullCard.blade.php
+++ resources/views/shared/fullCard.blade.php
@@ -1,7 +1,7 @@
 <div class="col-12 displayed_wisdom_{{$wisdom->id}}" id="current_wisdom" style="display: none;">
     <blockquote class="quote-box">
         <p class="quotation-mark">“</p>
-        <p class="quote-text">{{adjustLineBreaks($wisdom->text, false)}}</p>
+        <p class="quote-text" style="user-select:none;">{{adjustLineBreaks($wisdom->text, false)}}</p>
         <hr>
         <div class="d-flex justify-content-between">
             @foreach(json_decode($wisdom->categories) as $category)
