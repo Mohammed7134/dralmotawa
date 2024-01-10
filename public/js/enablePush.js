e1d587d4ede76ec65459882e4e8350c25eb510cc
Mohammed7134 <61147742+Mohammed7134@users.noreply.github.com>
2024-01-10 07:24:37 +0300

    test3

--- public/js/enablePush.js
+++ public/js/enablePush.js
@@ -46,8 +46,8 @@ notificationButton.addEventListener('click', function () {
     })
         .then((permissionResult) => {
             if (permissionResult !== 'granted') {
-                alert('نأسف٬ لا يمكن تفعيل الإشعارات في هذا المتصفح.');
-                // alert("يجب تثبيت الموقع أولا عن طريق إضافته للصفحة الرئيسية.");
+                // alert('نأسف٬ لا يمكن تفعيل الإشعارات في هذا المتصفح.');
+                alert("يجب تثبيت الموقع أولا عن طريق إضافته للصفحة الرئيسية.");
             }
             subscribeUser();
         })
